7cb0772696a947e1a46b276b5774f07489a8ec9c
Ralf Grubenmann <ralf.grubenmann@gmail.com>
2024-09-18 20:27:17 +0200

    implement register writing and reading

---
+++ src/error.rs
@@ -0,0 +1,32 @@
+#[derive(Debug)]
+pub enum Error<E> {
+    /// Some error originating from the communication bus
+    BusError(E),
+    /// Some error resulting from interacting with the device
+    SensorError(SensorError),
+}
+
+#[derive(Debug)]
+pub enum SensorError {
+    /// The chip at the specified address is not reporting the correct self
+    /// identification code.
+    ///
+    /// For I²C this is most likely if the ID change jumper is in the wrong
+    /// state or there is anther chip on the bus with this address.
+    BadChip,
+    /// Returned if the register bank is set to a invalid value
+    ///
+    /// There are 4 banks, 0-3
+    BankOutOfRange,
+    /// Attempted to write to a read-only register
+    WriteToReadOnly,
+    /// Attempted to create an AccelRange or GyroRange enum from an invalid
+    /// discriminant
+    InvalidDiscriminant,
+}
+
+impl<E> From<SensorError> for Error<E> {
+    fn from(err: SensorError) -> Self {
+        Error::SensorError(err)
+    }
+}
